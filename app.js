00a006ecf3d7bd3c496cb374d5120db46cb05677
ivanlougan <robinio123@gmail.com>
2024-10-17 16:08:22 +0100

    create /api/articles/:articles_id/comments endpoint to get all the comments of the corresponding article

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const { getTopics } = require('./controllers/getTopics');
 const { getEndpoints } = require('./controllers/getEndpoints');
 const { getArticleById } = require('./controllers/getArticleById');
 const { getArticles } = require('./controllers/getArticles')
+const { getCommentsByArticleId } = require('./controllers/getCommentsByArticleId')
 const {psqlInvalidIdErrorHandler, customErrorHandler, internalServerError} = require('./error-handlers')
 
 app.use(express.json());
@@ -17,6 +18,8 @@ app.get('/api/topics', getTopics);
 app.get('/api/articles/:article_id', getArticleById);
 
 app.get('/api/articles', getArticles);
+
+app.get('/api/articles/:article_id/comments', getCommentsByArticleId);
 
 
 
