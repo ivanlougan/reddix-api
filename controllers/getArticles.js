6a4d29b40fd4fac69cb797e10e6814c7aa0d26c2
ivanlougan <robinio123@gmail.com>
2024-10-22 16:18:11 +0100

    complete get articles and get articles by id endpoints

--- controllers/getArticles.js
+++ controllers/getArticles.js
@@ -1,10 +1,14 @@
 const fetchArticles = require('../models/fetchArticles')
 
 function getArticles(request, response, next) {
-    const { sort_by = 'created_at', order = 'DESC' } = request.query;
-    fetchArticles(sort_by, order).then((articles) => {
-        response.status(200).send(articles);
-    })
+    const { sort_by = "created_at", order = "DESC", topic } = request.query;
+    fetchArticles(sort_by, order, topic)
+      .then((articles) => {
+        response.status(200).send({ articles });
+      })
+      .catch((err) => {
+        next(err);
+      });
 }
 
 module.exports = { getArticles }
--- models/fetchArticles.js
+++ models/fetchArticles.js
@@ -1,20 +1,43 @@
 const db = require("../db/connection")
 
-function fetchArticles(sort_by = "created_at", order = "DESC") {
+function fetchArticles(sort_by = "created_at", order = "DESC", topic) {
     const validCols = ["comment_count", "created_at", "article_id", "title", "votes", "author", "topic"];
     const validOrder = ["ASC", "DESC"];
   
     if (!validCols.includes(sort_by)) sort_by = "created_at";
     if (!validOrder.includes(order)) order = "DESC";
-    
-    return db.query(
-        `SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.created_at, articles.votes, articles.article_img_url, 
-        COUNT(comments.comment_id):: INT AS comment_count 
-        FROM articles JOIN comments ON comments.article_id = articles.article_id 
-        GROUP BY articles.article_id
-        ORDER BY ${sort_by} ${order}`).then(({ rows }) => {            
-            return rows;
-        });
+  
+    const queryVals = [];
+  
+    let queryStr = `
+    SELECT
+      articles.author,
+      articles.title,
+      articles.article_id,
+      articles.topic,
+      articles.created_at,
+      articles.votes,
+      articles.article_img_url,
+    COUNT(comments.comment_id) ::INT AS comment_count
+    FROM comments
+    RIGHT JOIN articles ON comments.article_id = articles.article_id
+    `;
+  
+    if (topic) {
+      queryStr += ` WHERE articles.topic = $1`;
+      queryVals.push(topic);
+    }
+  
+    queryStr += `
+      GROUP BY articles.article_id
+      ORDER BY ${sort_by} ${order}
+    `;
+    return db.query(queryStr, queryVals).then(({ rows }) => {
+      if (rows.length === 0 && topic) {
+        return Promise.reject({ status: 404, message: "Topic Not Found" });
+      }
+      return rows;
+    });
 }
 
 module.exports = fetchArticles
