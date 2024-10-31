6a3cec9b076fcd6d3091c99d0e3659c9e25b2788
ivanlougan <robinio123@gmail.com>
2024-10-31 16:58:49 +0000

    deployment changes

--- listener.js
+++ listener.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const { PORT = 9090 } = process.env;
 
-app.listen(7070, () => {
-    console.log("Server is listening on port 7070...");
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}...`);
   });
